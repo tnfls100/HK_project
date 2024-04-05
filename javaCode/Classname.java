bbcbeff0a004b6c379ad744c335d917de2cb9175
Su <tnfls100@naver.com>
2024-04-05 19:48:42 +0900

    javaStart

---
+++ javaCode/Classname.java
@@ -0,0 +1,26 @@
+
+
+public class Classname{}
+
+/**
+ * InnerClassname
+ */
+
+
+/*
+ * VS Code에서
+ * Class나 main 작성하는 법
+ * 1. public class $Classname {}
+: 편집기에 class를 입력하면 토글 메뉴가 나타남
+토글 메뉴 중 사각형 아이콘을 클릭하면 Template
+   2. public static void main(String[] args) {}
+:편집기에 main을 입력한 다음 토글 메뉴에서 main을 선택.
+ * 
+ */
+public class Classname {
+
+    public static void main(String[] args) {
+        
+    }    
+}
+
